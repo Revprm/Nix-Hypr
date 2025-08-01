b01ee8f76ef6269c6e1bde8dc20fc78461515e70
installer <installer@gmail.com>
2025-08-01 17:34:42 +0700

    initial commit

---
+++ hosts/default/variables.nix
@@ -0,0 +1,20 @@
+# 💫 https://github.com/JaKooLit 💫 #
+# Variables
+
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "revprm";
+  gitEmail = "revy.pramana@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser =
+    "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
---
+++ hosts/prm/users.nix
@@ -0,0 +1,132 @@
+# 💫 https://github.com/JaKooLit 💫 #
+# Users - NOTE: Packages defined on this will be on current user only
+
+{ pkgs, username, ... }:
+
+let inherit (import ./variables.nix) gitUsername;
+in {
+  users = {
+    mutableUsers = true;
+    users."${username}" = {
+      homeMode = "755";
+      isNormalUser = true;
+      description = "${gitUsername}";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "libvirtd"
+        "scanner"
+        "lp"
+        "video"
+        "input"
+        "audio"
+      ];
+
+      # define user packages here
+      packages = with pkgs; [
+        # Developer Tools
+        vscode
+        gcc
+        gnumake
+        docker
+        docker-compose
+        tree
+        htop
+        obs-studio
+        postman
+
+        # NodeJS
+        nodejs
+        nodePackages.npm
+        nodePackages.yarn
+        nodePackages.typescript
+        nodePackages.typescript-language-server
+
+        # Python
+        python3
+        python3Packages.pip
+        python3Packages.virtualenv
+
+        # Rust
+        rustc
+        cargo
+
+        # PHP
+        php
+        php83Packages.composer
+        php83Packages.psalm
+
+        # Golang
+        go
+        gopls
+        delve
+
+        # CI/CD
+        gh
+
+        # Gaming
+        steam
+        lutris
+        heroic
+        protonup-qt
+
+        # Cysec Stuffs
+        wireshark
+        nmap
+        binwalk
+        john
+        hashcat
+        foremost
+        steghide
+        stegseek
+        zsteg
+        gdb
+
+        # Animanga
+        ani-cli
+        manga-tui
+      ];
+    };
+
+    defaultUserShell = pkgs.zsh;
+  };
+
+  environment.shells = with pkgs; [ zsh ];
+  environment.systemPackages = with pkgs; [ lsd fzf ];
+
+  programs = {
+    # Zsh configuration
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      ohMyZsh = {
+        enable = true;
+        plugins = [ "git" ];
+        theme = "robbyrussell";
+      };
+
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+
+      promptInit = ''
+        fastfetch -c $HOME/.config/fastfetch/config.jsonc
+
+        #pokemon colorscripts like. Make sure to install krabby package
+        #krabby random --no-mega --no-gmax --no-regional --no-title -s; 
+
+        # Set-up icons for files/directories in terminal using lsd
+        alias ls='lsd'
+        alias l='ls -l'
+        alias la='ls -a'
+        alias lla='ls -la'
+        alias lt='ls --tree'
+
+        source <(fzf --zsh);
+        HISTFILE=~/.zsh_history;
+        HISTSIZE=10000;
+        SAVEHIST=10000;
+        setopt appendhistory;
+      '';
+    };
+  };
+}
