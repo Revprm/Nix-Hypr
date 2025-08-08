57b28ec8e2d2d7d1ebcbac14653dcf56483b5610
revprm <revy.pramana@gmail.com>
2025-08-08 07:06:31 +0700

    fix: disable Bluetooth power on boot to improve system startup performance

--- hosts/prm/config.nix
+++ hosts/prm/config.nix
@@ -263,7 +263,7 @@ in {
   hardware = {
     bluetooth = {
       enable = true;
-      powerOnBoot = true;
+      powerOnBoot = false;
       settings = {
         General = {
           Enable = "Source,Sink,Media,Socket";
