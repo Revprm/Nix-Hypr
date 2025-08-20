0fb623bbbb5c107fbc755feff9a4cb58c5edd5bc
revprm <revy.pramana@gmail.com>
2025-08-20 09:09:53 +0700

    feat: create default NixOS module configuration with driver imports

---
+++ modules/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./amd-drivers.nix
+    ./intel-drivers.nix
+    ./local-hardware-clock.nix
+    ./nvidia-drivers.nix
+    ./nvidia-prime-drivers.nix
+    ./quickshell.nix
+    ./vm-guest-services.nix
+  ];
+}
